d0a94344fb5a114ae4172addb9359c2a7cbd9405
Felipe Catania <felicatania@gmail.com>
2018-04-12 10:03:55 -0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: path.join(__dirname, '/node_modules'),
+        include: path.join(__dirname, '/client/src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+      // {
+      //   test: /\.css$/,
+      //   exclude: path.join(__dirname, '/node_modules'),
+      //   include: path.join(__dirname, '/client/src'),
+      //   use: [ 'style-loader', 'css-loader' ]
+      // }
+    ]
+  }
+};
